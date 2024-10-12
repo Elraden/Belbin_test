74c377cef6e4dcdd201e3e39a95f28bce8278b61
Elraden <morozovsa94@yandex.ru>
2024-10-12 15:01:26 +0300

    Фикс пути к dist

--- server/server.js
+++ server/server.js
@@ -67,10 +67,10 @@ app.post('/api/save', (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
